9d41cd52c3643cf16d35733d10fa75fb10972238
Edrich0902 <edrichbarnard@gmail.com>
2020-04-06 12:41:15 +0200

    1st Android Commit. Only contains empty activity with background. Once done with the designs the designs will be uploaded

---
+++ CompulinkApp/app/src/main/java/com/example/compulinkapp/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.compulinkapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
