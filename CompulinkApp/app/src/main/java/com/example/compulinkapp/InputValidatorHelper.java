a39cbd356adc97b68e5d2acf73147c9b786529a3
nickuun <nickuun@gmail.com>
2020-04-22 20:15:14 -0700

    InputHelperValidation class created with methods in order to streamline the validation process. Validation Implimented in Registration Page

---
+++ CompulinkApp/app/src/main/java/com/example/compulinkapp/InputValidatorHelper.java
@@ -0,0 +1,143 @@
+package com.example.compulinkapp;
+
+import android.text.TextUtils;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class InputValidatorHelper {
+
+    // - InputValidatorHelper - Start
+
+    public String isEmail(TextView tv) {
+        String message = "null";
+        String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+        String email = tv.getText().toString();
+
+        if (email.matches(regex) == true){
+            message = "true"; //true
+        } else {
+            message = "false"; //false
+        }
+
+        return message;
+    }
+
+    public String isNumeric(TextView tv) { //This method tests if the entry String are all digits - Working (should change to boolean)
+
+        String message = "Null";
+        String textView = tv.getText().toString();
+
+        try {
+            Double.parseDouble(textView);
+            message = "true"; // true - is Numeric
+        } catch(NumberFormatException e){
+            message = "false"; // false - is not numeric
+        }
+        return message;
+    }
+
+    public String isEmpty(TextView tv) {  //This method tests if the field is empty - Working (should change to boolean)
+
+        String message = "Null";
+        if (tv.getText().toString().isEmpty()) {
+            message = "true"; //True
+        } else {
+            message = "false"; //False
+        }
+        return message;
+    }
+
+    public String lengthCheckMax(TextView tv , int maxSize) {//This method tests the length of the data against the max size it is allowed. - Working (should change to boolean)
+
+        String message = "Null";
+        int maximumSize = maxSize;
+        String textView = tv.getText().toString();
+        int length = textView.length();
+
+        if (length > maximumSize){
+            message = "false"; //False - The string is bigger than it is allowed to be
+        } else {
+            message = "true"; //True - The string size fits the criteria
+        }
+        return message;
+    }
+    public String lengthCheckMin(TextView tv , int minSize) {//This method tests the length of the data against the max size it is allowed. - Working (should change to boolean)
+
+        String message = "Null";
+        int minimumSize = minSize;
+        String textView = tv.getText().toString();
+        int length = textView.length();
+
+        if (length < minimumSize){
+            message = "false"; //False - The string is bigger than it is allowed to be
+        } else {
+            message = "true"; //True - The string size fits the criteria
+        }
+        return message;
+    }
+    public String falsify() {//This method ensures the validation string is false.
+        return "false";
+    }
+
+    public CharSequence appendToast(CharSequence toastPrint , String extraToast) {//This method ensures the validation string is false.
+        CharSequence tp = toastPrint + "\n" + extraToast;
+        return tp;
+    }
+    public String containsInt(TextView textView) {//This method ensures the validation string contains at least one int.
+        String message = "null";
+        String tv = textView.getText().toString();
+        int tvLength = tv.length();
+        int loop = 1;
+        char temp;
+        message = "false";
+
+        while (loop == 1){
+            for (int i = 0; i < tvLength; i++) {
+                temp = tv.charAt(i);
+                if (temp == '1' || temp == '2' || temp == '3' || temp == '4' || temp == '5'  //There must be an easier way to do this...
+                    || temp == '6' || temp == '7' || temp == '8' || temp == '9' || temp == '0')
+                {
+                    message = "true";
+                    loop --;
+                }
+            }
+            loop --;
+        }
+        return message;
+    }
+
+    public String containsUpperCase(TextView textView) {//This method ensures the validation string contains at least one upper-case character.
+        String message = "null";
+        String tv = textView.getText().toString();
+        int tvLength = tv.length();
+        int loop = 1;
+        char temp;
+
+        message = "false"; // false - Does not contain an upper-case character.
+
+        while (loop == 1){
+
+            for (int i = 0; i < tvLength; i++) {
+                temp = tv.charAt(i);
+                if (temp != '1' && temp != '2' && temp != '3' && temp != '4' && temp != '5'  //There must be an easier way to do this...
+                        && temp != '6' && temp != '7' && temp != '8' && temp != '9' && temp != '0')
+                {
+                    if (temp == tv.toUpperCase().charAt(i)){
+                        message = "true"; //There is an upper-case character.
+                        loop --;
+                    }
+                }
+            }
+            loop --;
+
+        }
+
+        return message;
+    }
+
+    // - InputValidatorHelper - End
+
+}
+
+
--- CompulinkApp/app/src/main/java/com/example/compulinkapp/RegisterAccount.java
+++ CompulinkApp/app/src/main/java/com/example/compulinkapp/RegisterAccount.java
@@ -2,14 +2,272 @@ package com.example.compulinkapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterAccount extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_account);
-    }
-}
+
+        // - Assigning Inputs - Start
+
+        // - Contact Information - Start
+        final TextView username = (TextView) findViewById(R.id.usernameRegInp);
+        final TextView password = (TextView) findViewById(R.id.passwordRegInp);
+        final TextView firstname = (TextView) findViewById(R.id.firstNameInp);
+        final TextView lastname = (TextView) findViewById(R.id.lastNameInp);
+        final TextView email = (TextView) findViewById(R.id.emailRegInp);
+        final TextView phone = (TextView) findViewById(R.id.phoneRegInp);
+        // - Contact Information - End
+        // - Address Information - Start
+        final TextView number = (TextView) findViewById(R.id.streetNumRegInp);
+        final TextView name = (TextView) findViewById(R.id.adressRegInp); //Note 1 'd' - Needs revising
+        final TextView city = (TextView) findViewById(R.id.citySuburbRegInp);
+        final TextView postal = (TextView) findViewById(R.id.postalCodeRegInp);
+        final TextView addInfo = (TextView) findViewById(R.id.additionalInfoRegInp);
+        // - Address Information - End
+        // - Assigning Inputs - End
+
+        //Register Button Code - Start
+        Button regBut = (Button) findViewById(R.id.registerBut);
+
+        regBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                InputValidatorHelper helper = new InputValidatorHelper();
+
+                //Variables for Toast - Start
+               // String validity = "true"; // We will assume the TextViews as valid and check against criteria.
+                String validity = "true"; //true actually
+                Context context = getApplicationContext();
+                CharSequence toastPrint = "The following validation criteria are not met:";
+                int durationToast = Toast.LENGTH_LONG;
+                //Variables for Toast - End
+
+                //Testing Validation - REMOVE
+
+
+
+                //Testing Validation - REMOVE
+
+
+ //Validation - Start
+                //Username Validation - Start
+                if (helper.isEmpty(username).equals("true")){
+                    toastPrint = helper.appendToast(toastPrint ,"Username - Should not be empty." );
+                    validity = helper.falsify();
+                } else {
+                    if(helper.lengthCheckMin(username, 4).equals("false")){
+                        toastPrint = helper.appendToast(toastPrint ,"Username - Should be at least 4 characters long." );
+                        validity = helper.falsify();
+                    } else {
+                        if(helper.lengthCheckMax(username, 18).equals("false")){
+                            toastPrint = helper.appendToast(toastPrint ,"Username - Should not exceed 18 characters in length." );
+                            validity = helper.falsify();
+                        } else {
+                            if(helper.containsInt(username).equals("true")){
+                                toastPrint = helper.appendToast(toastPrint ,"Username - Should not contain any integers." );
+                                validity = helper.falsify();
+                            }
+                        }
+                    }
+                }
+                //Username Validation - End
+                //Password Validation - Start
+                if (helper.isEmpty(password).equals("true")){
+                    toastPrint = helper.appendToast(toastPrint ,"Password - Should not be empty." );
+                    validity = helper.falsify();
+                } else {
+                    if (helper.lengthCheckMin(password, 8).equals("false")){
+                        toastPrint = helper.appendToast(toastPrint ,"Password - Should be at least 8 characters." );
+                        validity = helper.falsify();
+                    } else{
+                        if (helper.lengthCheckMax(password , 15).equals("false")){
+                            toastPrint = helper.appendToast(toastPrint ,"Password - Should not exceed 15 characters in length." );
+                            validity = helper.falsify();
+                        } else{
+                            if(helper.containsInt(password).equals("false")){
+                                toastPrint = helper.appendToast(toastPrint ,"Password - Should contain an integer" );
+                                validity = helper.falsify();
+                            } else {
+                                if (helper.containsUpperCase(password).equals("false")){
+                                    toastPrint = helper.appendToast(toastPrint ,"Password - Should contain an upper-case character" );
+                                    validity = helper.falsify();
+                                }
+                            }
+                        }
+                    }
+                }
+                //Password Validation - End
+                //FirstName Validation - Start
+                if (helper.isEmpty(firstname).equals("true")){
+                    toastPrint = helper.appendToast(toastPrint ,"First Name - Should not be empty." );
+                    validity = helper.falsify();
+                } else {
+                    if (helper.lengthCheckMax(firstname , 18).equals("true")){
+                        toastPrint = helper.appendToast(toastPrint ,"First Name - Should not exceed 18 characters." );
+                        validity = helper.falsify();
+                    } else{
+                        if(helper.containsInt(firstname).equals("true")){
+                            toastPrint = helper.appendToast(toastPrint ,"First Name - Should not exceed 18 characters." );
+                            validity = helper.falsify();
+                        }
+                    }
+                }
+                //FirstName Validation - End
+                //LastName Validation - Start
+                if (helper.isEmpty(lastname).equals("true")){
+                    toastPrint = helper.appendToast(toastPrint ,"Last Name - Should not be empty." );
+                    validity = helper.falsify();
+                } else{
+                    if (helper.lengthCheckMax(lastname , 18).equals("false")){
+                        toastPrint = helper.appendToast(toastPrint ,"Last Name - Should not exceed 18 characters." );
+                        validity = helper.falsify();
+                    } else {
+                        if(helper.containsInt(lastname).equals("true")){
+                            toastPrint = helper.appendToast(toastPrint ,"Last Name - Should not contain an integer." );
+                            validity = helper.falsify();
+                        }
+                    }
+                }
+                //LastName Validation - End
+                //eMail Validation - Start
+                if (helper.isEmpty(email).equals("true")){
+                    toastPrint = helper.appendToast(toastPrint ,"e-Mail - Should not be empty." );
+                    validity = helper.falsify();
+                } else {
+                    if (helper.isEmail(email).equals("false")){
+                        toastPrint = helper.appendToast(toastPrint ,"e-Mail - Should match standard e-mail patterns." );
+                        validity = helper.falsify();
+                    }
+                }
+                //eMail Validation - End
+                //phoneNumber Validation - Start
+                if (helper.isEmpty(phone).equals("true")){
+                    toastPrint = helper.appendToast(toastPrint ,"phoneNumber - Should not be empty." );
+                    validity = helper.falsify();
+                } else {
+                    if (helper.isNumeric(phone).equals("false")){
+                        toastPrint = helper.appendToast(toastPrint ,"phoneNumber - Should be numeric.");
+                        validity = helper.falsify();
+                    } else {
+                        if (helper.lengthCheckMin(phone,10).equals("false")){
+                            toastPrint = helper.appendToast(toastPrint ,"phoneNumber - Should be 10 digits long.");
+                            validity = helper.falsify();
+                        }else {
+                            if (helper.lengthCheckMax(phone, 12).equals("false")){
+                                toastPrint = helper.appendToast(toastPrint ,"phoneNumber - Length should smaller than 13 digits.");
+                                validity = helper.falsify();
+                            }
+                        }
+                    }
+                }
+                //phoneNumber Validation - End
+          //Address Validation - Start
+                //House number validation - Start
+                if (helper.isEmpty(number).equals("true")){
+                    toastPrint = helper.appendToast(toastPrint ,"House Number - Should not be null." );
+                    validity = helper.falsify();
+                } else {
+                    if (helper.isNumeric(number).equals("false")){
+                        toastPrint = helper.appendToast(toastPrint ,"House Number - Should be numeric." );
+                        validity = helper.falsify();
+                    } else {
+
+                    }  if (helper.lengthCheckMax(number , 6).equals("false")){
+                        toastPrint = helper.appendToast(toastPrint ,"House Number - Should not be longer than 6 characters long." );
+                        validity = helper.falsify();
+                    }
+
+                }
+
+                //House number validation - End
+                //Street name validation -  Start
+                if (helper.isEmpty(name).equals("true")){
+                    toastPrint = helper.appendToast(toastPrint ,"Street Name - Should not be null." );
+                    validity = helper.falsify();
+                } else {
+                    if(helper.lengthCheckMin(name , 2).equals("false")){
+                        toastPrint = helper.appendToast(toastPrint ,"Street Name - Should be at least 2 characters long." );
+                        validity = helper.falsify();
+                    }else{
+                        if (helper.lengthCheckMax(name , 18).equals("false")){
+                            toastPrint = helper.appendToast(toastPrint ,"Street Name - Should not be longer than 18 characters." );
+                            validity = helper.falsify();
+                        } else {
+                            if(helper.containsInt(name).equals("true")){
+                                toastPrint = helper.appendToast(toastPrint ,"Street Name - Should not contain integers." );
+                                validity = helper.falsify();
+                            }
+                        }
+                    }
+                }
+                //Street name validation -  End
+                //City validation -  Start
+                if (helper.isEmpty(city).equals("true")){
+                    toastPrint = helper.appendToast(toastPrint ,"City - Should not be null." );
+                    validity = helper.falsify();
+                } else {
+                    if(helper.lengthCheckMin(city , 2).equals("false")){
+                        toastPrint = helper.appendToast(toastPrint ,"City - Should be at least 2 characters long." );
+                        validity = helper.falsify();
+                    }else{
+                        if (helper.lengthCheckMax(city , 18).equals("false")){
+                            toastPrint = helper.appendToast(toastPrint ,"City - Should not be longer than 18 characters." );
+                            validity = helper.falsify();
+                        } else {
+                            if (helper.containsInt(city).equals("true")){
+                                toastPrint = helper.appendToast(toastPrint ,"City - Should not contain an integer." );
+                                validity = helper.falsify();
+                            }
+                        }
+                    }
+                }
+                //City validation -  End
+                //Postal Code validation -  Start
+                if (helper.isEmpty(postal).equals("true")){
+                    toastPrint = helper.appendToast(toastPrint ,"Postal Code - Should not be null." );
+                    validity = helper.falsify();
+                } else {
+                    if(helper.lengthCheckMin(postal , 4).equals("false")){
+                        toastPrint = helper.appendToast(toastPrint ,"Postal Code - Should not be less than 4 characters long." );
+                        validity = helper.falsify();
+                    }else{
+                        if (helper.lengthCheckMax(postal , 5).equals("false")){
+                            toastPrint = helper.appendToast(toastPrint ,"Postal Code - Should not be longer than 5 characters." );
+                            validity = helper.falsify();
+                        } else {
+                            if (helper.isNumeric(postal).equals("false")){
+                                toastPrint = helper.appendToast(toastPrint ,"Postal Code - Should consist of integers." );
+                                validity = helper.falsify();
+                            }
+                        }
+                    }
+                }
+                //Postal Code validation -  End
+          //Address Validation - Start
+ //Validation - End
+
+//Print Validity - Start
+                if(validity.equals("true")){
+                    toastPrint = "Successfully Validated - Registering Account.";
+                }
+
+                Toast toast = Toast.makeText(context, toastPrint, durationToast);
+                toast.show();
+//Print Validity - End
+
+            }
+        });//Register Button Code - End
+
+    } //onCreate End
+
+} //End - Register Account Class
