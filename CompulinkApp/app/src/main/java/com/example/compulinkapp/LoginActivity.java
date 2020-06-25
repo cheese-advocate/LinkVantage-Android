784a8f1abc34e7ac8e361d4d8d647072e73b8c3e
Edrich0902 <edrichbarnard@gmail.com>
2020-06-25 15:44:35 +0200

    Added some validation to the login of the app. Only checks for empty inputs

--- CompulinkApp/app/src/main/java/com/example/compulinkapp/LoginActivity.java
+++ CompulinkApp/app/src/main/java/com/example/compulinkapp/LoginActivity.java
@@ -7,7 +7,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -46,8 +48,36 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent dash = new Intent(v.getContext(), Dash.class);
-                startActivity(dash);
+                if(loginValidation())
+                {
+                    startActivity(dash);
+                }
+                else Toast.makeText(getApplicationContext(), "Invalid login", Toast.LENGTH_LONG).show();
             }
         });
     }
+
+    private boolean loginValidation()
+    {
+        boolean valid;
+
+        TextView usernameInp = findViewById(R.id.usernameInput);
+        TextView passwordInp = findViewById(R.id.pwInp);
+
+        if(usernameInp.getText().toString().trim().equals(""))
+        {
+            valid = false;
+        }
+        else if(passwordInp.getText().toString().trim().equals(""))
+        {
+            valid = false;
+        }
+        else valid = true;
+
+        if(valid)
+        {
+            return true;
+        }
+        else return false;
+    };
 }
