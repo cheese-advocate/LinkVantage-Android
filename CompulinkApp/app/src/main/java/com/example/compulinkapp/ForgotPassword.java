ac959648247b4f7bab6c5d98d319c50365b153ab
Edrich0902 <edrichbarnard@gmail.com>
2020-06-26 12:32:27 +0200

    New password page created and working. Still no validation but it is functional. Only thing needed is successful communication with the PHP web server to access the database

--- CompulinkApp/app/src/main/java/com/example/compulinkapp/ForgotPassword.java
+++ CompulinkApp/app/src/main/java/com/example/compulinkapp/ForgotPassword.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class ForgotPassword extends AppCompatActivity {
 
@@ -13,6 +14,8 @@ public class ForgotPassword extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        Toast.makeText(getApplicationContext(), "To reset your password an OTP will be sent to you either via\nEmail or\nTo your account on the website", Toast.LENGTH_LONG).show();
 
         Button returnToLogin = (Button) findViewById(R.id.backToLogin);
 
@@ -23,6 +26,16 @@ public class ForgotPassword extends AppCompatActivity {
             {
                 Intent backToLogin = new Intent(v.getContext(), LoginActivity.class);
                 startActivity(backToLogin);
+            }
+        });
+
+        Button resetPasswordSubmit = (Button) findViewById(R.id.reset_password_submit);
+
+        resetPasswordSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newPassword = new Intent(v.getContext(), NewPassword.class);
+                startActivity(newPassword);
             }
         });
     }
