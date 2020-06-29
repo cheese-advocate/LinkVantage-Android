c9887c3abdf8cbaa753a0bdcd66c4baf27c6789f
Edrich0902 <edrichbarnard@gmail.com>
2020-06-30 00:21:47 +0200

    Made further layout changes and improvements to the dashboard

--- CompulinkApp/app/src/main/java/com/example/compulinkapp/ProfileFragment.java
+++ CompulinkApp/app/src/main/java/com/example/compulinkapp/ProfileFragment.java
@@ -1,13 +1,18 @@
 package com.example.compulinkapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment{
@@ -42,6 +47,28 @@ public class ProfileFragment extends Fragment{
             public void onClick(View v) {
                 Intent resetPwPage = new Intent(v.getContext(), NewPassword.class);
                 startActivity(resetPwPage);
+            }
+        });
+
+        View deleteAccount = view.findViewById(R.id.deleteAccount);
+
+        deleteAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which)
+                        {
+                            case DialogInterface.BUTTON_POSITIVE:
+                                break;
+                            case DialogInterface.BUTTON_NEGATIVE:
+                                break;
+                        }
+                    }
+                };
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext(), R.style.dialog_theme);
+                builder.setMessage("Are you sure?").setPositiveButton("Yes", dialogClickListener).setNegativeButton("Cancel", dialogClickListener).show();
             }
         });
     }
