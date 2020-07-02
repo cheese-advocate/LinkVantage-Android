b84bddb9e5684040dde9fe3504cd165c95a085b0
Edrich0902 <edrichbarnard@gmail.com>
2020-07-02 16:33:42 +0200

    Started with the class for generating content dynamically when the user performs specific actions

---
+++ CompulinkApp/app/src/main/java/com/example/compulinkapp/ContentGenerator.java
@@ -0,0 +1,76 @@
+package com.example.compulinkapp;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+
+public class ContentGenerator {
+    Context context;
+
+    /**
+     * Constructor to set the context for the class
+     * @param ctx
+     */
+    public ContentGenerator(Context ctx)
+    {
+        context = ctx;
+    }
+
+    /**
+     * Receives a dp value and converts that value into pixels
+     * Returns a pixel value
+     * @param dp
+     * @return
+     */
+    public int getPixels(int dp)
+    {
+        return (int) (dp * context.getResources().getDisplayMetrics().density);
+    }
+
+    /**
+     * Creates a new card to be added to the linear layout acting as contianer for the cards
+     *
+     * Requires the layout the card needs to be added to and the text to display within the card
+     * @param mainLayout
+     * @param text
+     */
+    public void createJobCard(LinearLayout mainLayout, String text)
+    {
+        //New job card to be added
+        CardView card = new CardView(context);
+
+        //Set layout parameters for new card
+        CardView.LayoutParams params = new CardView.LayoutParams(
+                CardView.LayoutParams.WRAP_CONTENT,
+                CardView.LayoutParams.MATCH_PARENT
+        );
+        //Design of the new card
+        params.setMarginStart(getPixels(10));
+        params.width = getPixels(150);
+        card.setLayoutParams(params);
+        card.setCardBackgroundColor(Color.parseColor("#CC373741"));
+
+        //Layout parameters for text
+        LinearLayout.LayoutParams paramsText = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        //Design the text view
+        TextView tv = new TextView(context);
+        paramsText.gravity = 4;
+        tv.setLayoutParams(paramsText);
+        tv.setText(text);
+        tv.setTextColor(Color.parseColor("#F3F3F3"));
+        tv.setTextSize(getPixels(8));
+        //Add text view to card
+        card.addView(tv);
+        //Add new card to layout
+        mainLayout.addView(card);
+    }
+}
--- CompulinkApp/app/src/main/java/com/example/compulinkapp/JobFragment.java
+++ CompulinkApp/app/src/main/java/com/example/compulinkapp/JobFragment.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
@@ -13,5 +16,21 @@ public class JobFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ((Dash) getActivity()).setActionBarTitle("Jobs");
         return inflater.inflate(R.layout.fragment_jobs, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button testBtn = view.findViewById(R.id.testBtn);
+        final ContentGenerator cg = new ContentGenerator(getContext());
+        final LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.jobs_todo);
+
+        testBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cg.createJobCard(linearLayout , "JOB Test");
+            }
+        });
     }
 }
