4d777dca8f478e501ace922767a0e44482a97a08
Edrich0902 <edrichbarnard@gmail.com>
2020-07-03 15:19:48 +0200

    Jobs can now be changed and moved from Pending, Active and Finished statuses. New jobs can be added. The jobs page is now refreshable and code can get executed on the refresh of the page. These are all functional but still only templates. When the content is received from the web server it can be used in the existing templates. All templates are ready for implementation

--- CompulinkApp/app/src/main/java/com/example/compulinkapp/ContentGenerator.java
+++ CompulinkApp/app/src/main/java/com/example/compulinkapp/ContentGenerator.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.graphics.Color;
 import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -13,18 +11,25 @@ import androidx.cardview.widget.CardView;
 
 public class ContentGenerator {
     Context context;
-
+    View view;
     /**
      * Constructor to set the context for the class
      * @param ctx
      */
-    public ContentGenerator(Context ctx)
+    public ContentGenerator(Context ctx, View view_sent)
     {
         context = ctx;
+        view = view_sent;
     }
 
     /**
      * Receives a dp value and converts that value into pixels
+     * When setting content programmatically the sizes are set in pixels
+     * Thus a dp value is provided and the method ensures a pixel value is added
+     * to the element's size
+     *
+     * This ensures that generated content is the same size are manually created content
+     *
      * Returns a pixel value
      * @param dp
      * @return
@@ -35,7 +40,10 @@ public class ContentGenerator {
     }
 
     /**
-     * Creates a new card to be added to the linear layout acting as contianer for the cards
+     * Creates a new card to be added to the linear layout that is acting as container for the cards
+     *
+     * The argument list still needs to be modified so that it accepts all content needed for a job
+     * This is just a working template for testing purposes
      *
      * Requires the layout the card needs to be added to and the text to display within the card
      * @param mainLayout
@@ -44,7 +52,7 @@ public class ContentGenerator {
     public void createJobCard(LinearLayout mainLayout, String text)
     {
         //New job card to be added
-        CardView card = new CardView(context);
+        final CardView card = new CardView(context);
 
         //Set layout parameters for new card
         CardView.LayoutParams params = new CardView.LayoutParams(
@@ -70,6 +78,21 @@ public class ContentGenerator {
         tv.setTextSize(getPixels(8));
         tv.setTextAlignment(TextView.TEXT_ALIGNMENT_GRAVITY);
         tv.setGravity(Gravity.CENTER);
+
+        /**
+         * Add onClickListener to the newly created card
+         * This ensures that newly added cards will also
+         * be able to move between the different job categories
+         */
+        card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final LinearLayout parent = (LinearLayout) card.getParent();
+                ContentGenerator cg = new ContentGenerator(context, view);
+                cg.changeParent(parent, card);
+            }
+        });
+
         //Add text view to card
         card.addView(tv);
         //Add new card to layout
@@ -80,11 +103,15 @@ public class ContentGenerator {
      * Changes the specified card view's parent. If the card needs to be displayed in
      * different location after user performs specific action
      *
-     * Requires the new parent and the view to be moved
-     * @param newParent
-     * @param view
+     * When card is in finished jobs - gets removed on click
+     * When card is in pending jobs - gets moved to active jobs on click
+     * When card is in active jobs - gets moved to finished jobs on click
+     *
+     * Requires the current of the view parent and the view to be moved
+     * @param parent - in linear layout format
+     * @param vw - in Card View format
      */
-    public void changeParent(LinearLayout newParent, CardView view)
+    public void changeParent(LinearLayout parent, CardView vw)
     {
         //Layout params need to be reset
         CardView.LayoutParams params = new CardView.LayoutParams(
@@ -94,15 +121,27 @@ public class ContentGenerator {
         params.setMarginStart(getPixels(10));
         params.width = getPixels(150);
         //Set the layout params
-        view.setLayoutParams(params);
-        //Get the current parent of the view
-        ViewGroup parent = (ViewGroup) view.getParent();
-        //Remove child from current parent
+        vw.setLayoutParams(params);
+        //checks if the parent given is null
         if(parent != null)
         {
-            parent.removeView(view);
+            //decides where to move the clicked view based on the view's parent
+            if(parent.getId() == R.id.jobs_active)
+            {
+                parent.removeView(vw);
+                LinearLayout newParent = (LinearLayout) view.findViewById(R.id.jobs_finished);
+                newParent.addView(vw);
+            }
+            else if(parent.getId() == R.id.jobs_pending)
+            {
+                parent.removeView(vw);
+                LinearLayout newParent = (LinearLayout) view.findViewById(R.id.jobs_active);
+                newParent.addView(vw);
+            }
+            else if(parent.getId() == R.id.jobs_finished)
+            {
+                parent.removeView(vw);
+            }
         }
-        //Give child the new specified parent
-        newParent.addView(view);
     }
 }
--- CompulinkApp/app/src/main/java/com/example/compulinkapp/JobFragment.java
+++ CompulinkApp/app/src/main/java/com/example/compulinkapp/JobFragment.java
@@ -1,5 +1,6 @@
 package com.example.compulinkapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,20 +12,35 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 public class JobFragment extends Fragment{
 
+    /**
+     * These are the methods and functions that execute when the view is being created
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ((Dash) getActivity()).setActionBarTitle("Jobs");
         return inflater.inflate(R.layout.fragment_jobs, container, false);
     }
 
+    /**
+     * These methods get executed after the view has been created
+     *
+     * @param view
+     * @param savedInstanceState
+     */
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        //Button used to test content addition functionality
         Button testBtn = view.findViewById(R.id.testBtn);
-        final ContentGenerator cg = new ContentGenerator(getContext());
+        final ContentGenerator cg = new ContentGenerator(getContext(), view);
         final LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.jobs_active);
 
         testBtn.setOnClickListener(new View.OnClickListener() {
@@ -34,13 +50,34 @@ public class JobFragment extends Fragment{
             }
         });
 
+        //Test card view used to test functionality of new code
         final CardView testV = view.findViewById(R.id.TEST);
-        final LinearLayout linearLayout_new = (LinearLayout) view.findViewById(R.id.jobs_pending);
 
         testV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                cg.changeParent(linearLayout, testV);
+                final LinearLayout parent = (LinearLayout) testV.getParent();
+                cg.changeParent(parent, testV);
+            }
+        });
+
+        final SwipeRefreshLayout swipe_refresh = view.findViewById(R.id.jobs_fragment_refreshSwipe);
+        //Set the look of the refresh icon while or when refreshing
+        swipe_refresh.setColorSchemeResources(R.color.colorAccent);
+        swipe_refresh.setProgressBackgroundColorSchemeResource(R.color.colorPrimary);
+        /**
+         * Called every time the page gets refreshed
+         *
+         * Code to contact web server/database to be added here. This will update the jobs on each refresh
+         */
+        swipe_refresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                //Code to execute on refresh required here
+
+                //Set refresh to false when code is complete
+                //This ensures the icon goes away when done refreshing
+                swipe_refresh.setRefreshing(false);
             }
         });
     }
