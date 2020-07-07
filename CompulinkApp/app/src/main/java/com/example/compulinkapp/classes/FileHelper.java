7eea235de68a1673eaddc07be09710293ddefa32
Edrich0902 <edrichbarnard@gmail.com>
2020-07-07 16:52:06 +0200

    Added a todo list to the home screen where technicians can set up tasks they need to do.

---
+++ CompulinkApp/app/src/main/java/com/example/compulinkapp/classes/FileHelper.java
@@ -0,0 +1,56 @@
+package com.example.compulinkapp.classes;
+
+import android.content.Context;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+public class FileHelper {
+    public static final String FILENAME = "listinfo.dat";
+
+    public static void writeData(ArrayList<String> items, Context ctx)
+    {
+        try
+        {
+            FileOutputStream fos = ctx.openFileOutput(FILENAME, ctx.MODE_PRIVATE);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(items);
+            oos.close();
+        } catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static ArrayList<String> readData(Context ctx)
+    {
+        ArrayList<String> itemsList = null;
+
+        try {
+            FileInputStream fis = ctx.openFileInput(FILENAME);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            itemsList = (ArrayList<String>) ois.readObject();
+        } catch (FileNotFoundException e)
+        {
+            itemsList = new ArrayList<String>();
+
+            e.printStackTrace();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+
+        return itemsList;
+    }
+}
--- CompulinkApp/app/src/main/java/com/example/compulinkapp/fragments/HomeFragment.java
+++ CompulinkApp/app/src/main/java/com/example/compulinkapp/fragments/HomeFragment.java
@@ -4,6 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,8 +18,14 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.compulinkapp.activities.DashActivity;
 import com.example.compulinkapp.R;
+import com.example.compulinkapp.classes.FileHelper;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment{
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> adapter;
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ((DashActivity) getActivity()).setActionBarTitle("Home");
@@ -24,26 +36,42 @@ public class HomeFragment extends Fragment{
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        final SwipeRefreshLayout swipe_refresh = view.findViewById(R.id.home_fragment_refreshSwipe);
-        //Set the look of the refresh icon while or when refreshing
-        swipe_refresh.setColorSchemeResources(R.color.colorAccent);
-        swipe_refresh.setProgressBackgroundColorSchemeResource(R.color.colorPrimary);
-        /**
-         * Called every time the page gets refreshed
-         *
-         * Code to contact web server/database to be added here. This will update the jobs on each refresh
-         */
-        swipe_refresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+        final EditText itemEntered = view.findViewById(R.id.list_item);
+        Button addItem = view.findViewById(R.id.list_add_btn);
+        ListView listView = view.findViewById(R.id.todo_list);
+
+        items = FileHelper.readData(getContext());
+        adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_expandable_list_item_1, items);
+        listView.setAdapter(adapter);
+
+        addItem.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onRefresh() {
-                //Code to execute on refresh required here
-                /**
-                 * Call database to see if new jobs were added
-                 * Or if jobs were removed or changed
-                 */
-                //Set refresh to false when code is complete
-                //This ensures the icon goes away when done refreshing
-                swipe_refresh.setRefreshing(false);
+            public void onClick(View v) {
+                String item = itemEntered.getText().toString();
+
+                if(item.trim().equals(""))
+                {
+                    Toast.makeText(getContext(), "No text entered", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    adapter.add(item);
+                    itemEntered.setText("");
+
+                    FileHelper.writeData(items, getContext());
+
+                    Toast.makeText(getContext(), "Item added", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                items.remove(position);
+                adapter.notifyDataSetChanged();
+                FileHelper.writeData(items, getContext());
+                Toast.makeText(getContext(), "Item Deleted", Toast.LENGTH_SHORT).show();
             }
         });
     }
