97305444e1940baf053b9c6505585df63cea947b
Edrich0902 <edrichbarnard@gmail.com>
2020-09-11 13:27:21 +0200

    Created a popup where new sales leads can be added. Saves space on the sales lead screen and makes the look and feel better

--- CompulinkApp/app/src/main/java/com/example/compulinkapp/fragments/SalesFragment.java
+++ CompulinkApp/app/src/main/java/com/example/compulinkapp/fragments/SalesFragment.java
@@ -1,10 +1,14 @@
 package com.example.compulinkapp.fragments;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -27,14 +31,14 @@ public class SalesFragment extends Fragment{
     @Override
     public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+        final Dialog popupDialog = new Dialog(getContext());
         /**
          * Initialisation of all the parent layouts to which the content can be added
          */
-        final LinearLayout clientParent = (LinearLayout) view.findViewById(R.id.client_container_main);
-        final LinearLayout potentialClientParent = (LinearLayout) view.findViewById(R.id.potential_clients_container);
-        final LinearLayout statParent = (LinearLayout) view.findViewById(R.id.stats_container);
-        final LinearLayout feedbackParent = (LinearLayout) view.findViewById(R.id.feedback_container);
+        final LinearLayout clientParent = view.findViewById(R.id.client_container_main);
+        final LinearLayout potentialClientParent = view.findViewById(R.id.potential_clients_container);
+        final LinearLayout statParent = view.findViewById(R.id.stats_container);
+        final LinearLayout feedbackParent = view.findViewById(R.id.feedback_container);
 
         /**
          * Setting the swipe refresh layout
@@ -109,6 +113,32 @@ public class SalesFragment extends Fragment{
                 //Call this method to create new client as many times as needed
                 //The strings needed to display should just be passed into the method
                 cg.createFeedbackCard(feedbackParent, "Edrich Barnard", "PC Repair", "Overall Good job. Great service");
+            }
+        });
+
+        /**
+         * Creates a popup window where the technician can add a new sales lead
+         */
+        Button popup = view.findViewById(R.id.popupTest);
+        popup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Set the layout to be displayed in the popup
+                popupDialog.setContentView(R.layout.sales_lead_popup);
+                //Initialise the close button/image
+                ImageView close = popupDialog.findViewById(R.id.close);
+                //OnClickListener for close button/image
+                close.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //Close the popup
+                        popupDialog.dismiss();
+                    }
+                });
+                //Prevent the popup from having a weird white border on the sides
+                popupDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                //Show the popup
+                popupDialog.show();
             }
         });
     }
